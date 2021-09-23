e8938f3367b55bd1eeb8e28c1ab973f617bdb7cb
lawencon.tomi <lawencon.tomi@bankmandiri.co.id>
2021-09-23 13:31:17 +0700

    limit requet per IP/Remote addr

---
+++ src/main/java/com/example/demo/model/Constants.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class Constants {
+    public static final String topic = "myFirst";
+    public static final String groupId = "testing";
+
+    public static final int MAX_REQUESTS_PER_SECOND = 5;
+}
+
