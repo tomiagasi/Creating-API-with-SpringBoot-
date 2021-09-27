42ef6db51e5ad4b5b9e802f3963fe6b215d3789b
lawencon.tomi <lawencon.tomi@bankmandiri.co.id>
2021-09-27 15:04:30 +0700

    Standarisasi error code & error message

--- src/main/java/com/example/demo/model/Constants.java
+++ src/main/java/com/example/demo/model/Constants.java
@@ -1,9 +1,20 @@
 package com.example.demo.model;
 
+import org.apache.kafka.common.protocol.types.Field;
+
 public class Constants {
     public static final String topic = "myFirst";
     public static final String groupId = "testing";
 
     public static final int MAX_REQUESTS_PER_SECOND = 5;
+
+    /*
+    ERROR CODE & MESSAGE
+     */
+    public static final String[] SUCCESS = {"00", "Success"};
+    public static final String[] USERNAME_EXIST = {"83", "Username already exist!"};
+    public static final String[] ROLE_NOT_AVAILABLE = {"84", "Role not available!"};
+    public static final String[] METHOD_ERROR = {"02", "Method Error!"};
+    public static final String[] INVALID_USERNAME_OR_PASSWORD = {"81", "Invalid Username or Password!"};
 }
 
---
+++ src/main/java/com/example/demo/model/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorCode {
+
+    private String code;
+
+    private String message;
+
+}
