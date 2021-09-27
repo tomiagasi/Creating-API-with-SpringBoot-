eed790579d83e189cd1457392188f144c0360290
lawencon.tomi <lawencon.tomi@bankmandiri.co.id>
2021-09-27 15:03:25 +0700

    Add header request from swagger

--- src/main/java/com/example/demo/security/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/security/SwaggerConfiguration.java
@@ -16,10 +16,41 @@ import java.util.*;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+	private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json"));
 
-    @Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
+	Contact contact = new Contact(
+			"",
+			"",
+			"");
+
+	List<VendorExtension> vext = new ArrayList<>();
+	ApiInfo apiInfo = new ApiInfo(
+			"Backend API",
+			"This is API from Tomi Agasi",
+			"1.0.0",
+			"",
+			contact,
+			"",
+			"",
+			vext);
+
+	@Bean
+	public Docket api() {
+		ParameterBuilder parameterBuilder = new ParameterBuilder();
+		parameterBuilder.name("Authorization")
+				.modelRef(new ModelRef("string"))
+				.parameterType("header")
+				.description("JWT token")
+				.required(false)
+				.build();
+		List<Parameter> parameters = new ArrayList<>();
+		parameters.add(parameterBuilder.build());
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
+				.produces(DEFAULT_PRODUCES_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_CONSUMES)
+				.select()
+				.build()
+				// Setting globalOperationParameters ensures that authentication header is applied to all APIs
+				.globalOperationParameters(parameters);
 	}
 }
