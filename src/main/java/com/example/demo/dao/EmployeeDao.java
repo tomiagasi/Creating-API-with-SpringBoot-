f5fc8f0c4f5bd3fad5680a331697657d5e39289d
lawencon.tomi <lawencon.tomi@bankmandiri.co.id>
2021-10-11 13:09:44 +0700

    Delete class Employee & Jabatan

--- src/main/java/com/example/demo/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeDao extends JpaRepository<Employee, Integer> {
-}
--- src/main/java/com/example/demo/dao/JabatanDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Jabatan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JabatanDao extends JpaRepository<Jabatan, Integer> {
-}
